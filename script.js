48f7f33ee49260fb79dbceb92fd85912ad7a77a1
Mateus da Silva Santos <mateussilvadev1@gmail.com>
2025-11-30 22:43:44 -0300

    final setting

--- script.js
+++ script.js
@@ -5,8 +5,14 @@ function toggleMode() {
   // pegar a  tag img
   const img = document.querySelector("#profile img")
 
+    if   (document.querySelector('#profile img')) {
+      img.setAttribute("src", "./assets/Avatar-main.png")
+    } 
+
+    else (document.querySelector('#profile img')); {
+      img.setAttribute("src", "./assets/Avatar-main.png")
   //decidi manter a mesma imagem em dark e light mode
   }
-
+}
 
 
